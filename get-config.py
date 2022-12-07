a556fa5eb3ddc2dd3ef6c7910aa251b5226c69ec
ChangeinX <userdata0@gmail.com>
2022-12-07 14:27:06 -0600

    Optional login paramter

--- get-config.py
+++ get-config.py
@@ -6,6 +6,17 @@ import sys
 import serial
 import time
 
+def login(ser, username, password):
+    # Wait for the device to send the login prompt
+    time.sleep(2)
+    ser.write(username.encode('ascii') + b"\r")
+
+    # Wait to get the prompt back for password
+    time.sleep(2)
+    ser.write(password.encode('ascii') + b"\r")
+    print("Authenticating... Please wait")
+    # If the device uses radius authentication, wait until it falls back to local authentication
+    time.sleep(20)
 
 def main():
     # Get the serial port from the command line arguments
@@ -14,7 +25,7 @@ def main():
     # get output file path as txt from command line arguments
     output_file = sys.argv[2]
 
-    # Authentication username and password as command line arguments
+    # Authentication username and password as optional command line arguments
     username = sys.argv[3]
     password = sys.argv[4]
 
@@ -24,15 +35,9 @@ def main():
         stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS
     )
 
-    # Login to the device, wait to get the prompt back for username
-    time.sleep(2)
-    ser.write(username.encode('ascii') + b"\r")
-
-    # Wait to get the prompt back for password
-    time.sleep(2)
-    ser.write(password.encode('ascii') + b"\r")
-
-    print("Authenticating... Please wait")
+    # Login to the device if username and password are provided
+    if username and password:
+        login(ser, username, password) # login function is defined above
     # If the device uses radius authentication, wait until it falls back to local authentication
     time.sleep(10)
     ser.write(password.encode('ascii') + b"\r")
@@ -57,6 +62,8 @@ def main():
 
     # Close the serial connection
     ser.close()
+    # example: python get-config.py /dev/ttyUSB0 running-config.txt admin admin or
+    # python get-config.py /dev/ttyUSB0 running-config.txt
 
 
 if __name__ == "__main__":
